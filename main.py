4dd2b1959babf9f843e2250f73ed50495700fb79
Seon47 <seon8717@gamil.com>
2024-08-31 11:42:40 +0900

    no message

--- main.py
+++ main.py
@@ -5,18 +5,18 @@ from data_loader import load_data
 def main():
     st.title("It's me'")
     
-df=load_data()
-  
+    df=load_data()
+    
 
-st.subheader('Select Date Range')
-df['Date']=pd.to_datetime(df['Date'])
-start_date =st.date_input('Start date',df['Date'].min())
-end_date=st.date_input("End date",df["Date"].max())
+    st.subheader('Select Date Range')
+    df['Date']=pd.to_datetime(df['Date'])
+    start_date =st.date_input('Start date',df['Date'].min())
+    end_date=st.date_input("End date",df["Date"].max())
 
-range_df= df[(df["Date"]>=pd.to_datetime(start_date)),
-             (df['Date']<=pd.to_datetime(end_date))]
-range_df= range_df.reset_index(drop=True)
-st.table(range_df)
+    range_df= df[(df["Date"]>=pd.to_datetime(start_date))&
+                (df['Date']<=pd.to_datetime(end_date))]
+    range_df= range_df.reset_index(drop=True)
+    st.table(range_df)
 
 if __name__ == '__main__':
     main()
